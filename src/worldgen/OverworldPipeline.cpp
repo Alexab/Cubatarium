1620cedb5fe9bf790337afcefe30dc516acf9d07
Alexab <palexab@gmail.com>
2026-05-30 22:29:51 +0300

    Add overworld height sampler and terrain pipeline implementations.
    Complete tier-1 Perlin fBM overworld, hills, and mountains pipeline classes used by the factory.

---
+++ src/worldgen/OverworldPipeline.cpp
@@ -0,0 +1,36 @@
+#include "OverworldPipeline.h"
+#include "WorldGenStages.h"
+
+namespace cutum {
+
+OverworldPipeline::OverworldPipeline(WorldGenContext ctx, HeightPreset preset)
+ : IWorldGenPipeline(ctx)
+ , heightSampler_(ctx.settings.seed, ctx.settings.seaLevel, ctx.settings.maxHeight, preset)
+ , preset_(preset)
+{
+}
+
+void OverworldPipeline::GenerateColumn(int worldX, int worldZ)
+{
+ const int surfaceY = heightSampler_.SurfaceYAt(worldX, worldZ);
+ ColumnLayerRule rule;
+ rule.surfaceBlock = ctx_.grass;
+ rule.subsurfaceBlock = ctx_.dirt;
+ rule.fillerBlock = ctx_.stone;
+
+ if (preset_ == HeightPreset::Mountains &&
+     heightSampler_.params().stoneSurfaceAboveY > 0 &&
+     surfaceY >= heightSampler_.params().stoneSurfaceAboveY) {
+  rule.surfaceBlock = ctx_.stone;
+  rule.subsurfaceBlock = ctx_.stone;
+ }
+
+ FillTerrainColumn(ctx_, worldX, worldZ, surfaceY, rule);
+}
+
+int OverworldPipeline::SurfaceYAt(int worldX, int worldZ) const
+{
+ return heightSampler_.SurfaceYAt(worldX, worldZ);
+}
+
+} // namespace cutum
