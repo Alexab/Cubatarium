1bc94e372fc53cf20456c6bbbf5f407c5127ffef
Alexab <palexab@gmail.com>
2026-06-15 17:53:20 +0300

    Hide Android system bars in immersive mode to reclaim HUD space.

--- platforms/android/app/src/main/java/com/cubatarium/MainActivity.java
+++ platforms/android/app/src/main/java/com/cubatarium/MainActivity.java
@@ -1,6 +1,12 @@
 package com.cubatarium;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.google.androidgamesdk.GameActivity;
 
@@ -12,6 +18,7 @@ public class MainActivity extends GameActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideSystemBars();
         nativeOnCreate();
         try {
             AssetExtractor.extractIfNeeded(this);
@@ -19,6 +26,35 @@ public class MainActivity extends GameActivity {
             android.util.Log.e("Cubatarium", "Asset extraction failed", e);
             throw e;
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemBars();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemBars();
+        }
+    }
+
+    private void hideSystemBars() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+            layoutParams.layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(layoutParams);
+        }
+        WindowInsetsControllerCompat controller =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+        controller.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
     }
 
     private native void nativeOnCreate();
